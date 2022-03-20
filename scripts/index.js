73d621acab81c1a455f70040fca067a48478de8b
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-03-20 21:24:50 +0300

    feat:display input values on form

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,39 @@
+// business logic
+function Purchase(size, flavor, crust, topping) {
+    this.size = size;
+    this.flavor = flavor;
+    this.crust = crust;
+    this.topping = topping;
+}
+
+// user interface logic
 $(document).ready(function () {
+    $("form#customerOrder").submit(function (event) {
+        event.preventDefault();
+
+        var inputtedSize = $("#sizeInput").val();
+        var inputtedFlavor =$("#flavorInput").val();
+        var inputtedCrust =$("#crustInput").val();
+        var inputtedTopping=$("#topping2").val();
+
+
+
+        var newOrder = new Purchase(inputtedSize, inputtedFlavor, inputtedCrust, inputtedTopping);
+
+        $("ul#outputValues").append("<li><span class='contact'>"+newOrder.size+ "</span></li>");
+        $("ul#outputValues").append("<li><span class='contact'>"+newOrder.flavor+ "</span></li>");
+        $("ul#outputValues").append("<li><span class='contact'>"+newOrder.crust+ "</span></li>");
+        $("ul#outputValues").append("<li><span class='contact'>"+newOrder.topping+ "</span></li>");
+
+        
+
+
+    })
+
+
+
+
+
     $(".delivery").click(function () {
         $(".deliveryDetails").toggle();
     });
