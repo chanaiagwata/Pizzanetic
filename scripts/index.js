b7d9b3112609f2fed3fe9c005a8cce4177a7f7aa
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-03-21 03:46:17 +0300

    chore:delete redundant code

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 function Purchase(size, crust, topping, totalPrice) {
     this.size = size;
     this.crust = crust;
@@ -8,103 +7,34 @@ function Purchase(size, crust, topping, totalPrice) {
 
 // user interface logic
 $(document).ready(function () {
-            $("form#customerOrder").submit(function (event) {
-                event.preventDefault();
+    $("form#customerOrder").submit(function (event) {
+        event.preventDefault();
 
-                let inputtedSize = $("#sizeInput").val();
-                    let inputtedCrust = $("#crustInput").val();
-                let inputtedTopping = [];
-                $.each($("input[name='topping1']:checked"), function () {
-                    inputtedTopping.push($(this).val()); 
-                
-                });
-                // let numbers = inputtedTopping;
-                // let sum1 = numbers.reduce((x,y)=>x+y);
-                // console.log(sum1);
-                // //  use switch statement as control flow for Purchase constructor
-                // switch (size) {
-                //     case "0":
-                //         amount = 0;
-                //         break;
-                //     case "Large":
-                //         amount = 1500;
-                //         console.log(amount);
-                //         break;
-                //     case "Medium":
-                //         amount = 900;
-                //         console.log(amount);
-                //         break;
-                //     case "Regular":
-                //         amount = 450;
-                //         console.log(amount);
-                //     default:
-                //         console.log("invalid");
-                // }
-                // switch (crust) {
-                //     case "0":
-                //         crustPrice = 0;
-                //         break;
-                //     case "Flatbread":
-                //         crustPrice = 50;
-                //         break;
-                //     case "Neapolitan pizza":
-                //         crustPrice = 100;
-                //         break;
-                //     case "Stuffed crust":
-                //         crustPrice = 150;
-                //         break;
-                //     case "Glutten-free":
-                //         crustPrice = 200;
-                //         break;
-                //     case "Sicilian":
-                //         crustPrice = 250;
-                //         break;
-                //     case "Deep-dish":
-                //         crustPrice = 300;
-                //         break;
-                //     default:
-                //         console.log("invalid")
-                // }
-                // let toppingCheckedValue = inputtedTopping.length * 30;
-                // console.log(toppingCheckedValue);
-                // let totalPrice = inputtedSize+ inputtedCrust + inputtedTopping;
-                // document.getElementById("#sum").innerHTML = totalPrice;
-                // if ((size == "0") && (crust == "0")) {
-                //     alert("Select appropriate pizza size and crust")
-                // } else {
-                 
-                // };
+        let inputtedSize = $("#sizeInput").val();
+        let inputtedCrust = $("#crustInput").val();
+        let inputtedTopping = [];
+        $.each($("input[name='topping1']:checked"), function () {
+            inputtedTopping.push($(this).val());
+        });
         let totalPrice = parseInt(inputtedSize) + parseInt(inputtedCrust) + parseInt(inputtedTopping);
         var newOrder = new Purchase(inputtedSize, inputtedCrust, inputtedTopping, totalPrice);
-        $("#outPutValues").append('</td><td id="selectSize">'+ newOrder.size + '</td><td id="selectCrust">'+newOrder.crust + '</td><td id="selectTopping">'+newOrder.topping+'</td><td id="total">'+newOrder.totalPrice+'</td></tr>');
-      console.log(newOrder.size);
-      console.log(newOrder.crust);
-      console.log(newOrder.topping);
-      console.log(newOrder.totalPrice)
-
-        // $("ul#outputValues").append("<li><span class='contact'>" + newOrder.size + "</span></li>");
-        // $("ul#outputValues").append("<li><span class='contact'>" + newOrder.flavor + "</span></li>");
-        // $("ul#outputValues").append("<li><span class='contact'>" + newOrder.crust + "</span></li>");
-        // $("ul#outputValues").append("<li><span class='contact'>" + newOrder.topping + "</span></li>");
-
-
-
-
+        $("#outPutValues").append('</td><td id="selectSize">' + newOrder.size + '</td><td id="selectCrust">' + newOrder.crust + '</td><td id="selectTopping">' + newOrder.topping + '</td><td id="total">' + newOrder.totalPrice + '</td></tr>');
+        console.log(newOrder.size);
+        console.log(newOrder.crust);
+        console.log(newOrder.topping);
+        console.log(newOrder.totalPrice)
 
         $("#sizeInput").val("");
         $("#flavorInput").val("");
         $("#crustInput").val("");
         $("outPutValues").append("")
 
+    });
+    $(".delivery").click(function () {
+        $(".deliveryDetails").toggle();
 
-
-    // })
-            });
-            $(".delivery").click(function () {
-                $(".deliveryDetails").toggle();
-                
-            });
-            $(".btn").click(function(){
-                alert("Your order has been received and will be delivered shortly")
-            })
-     });
+    });
+    $(".btn").click(function () {
+        alert("Your order has been received and will be delivered shortly")
+    })
+});
