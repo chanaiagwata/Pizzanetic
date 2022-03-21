f890564f919073dca4116b5763c5d938601f7a2c
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-03-21 03:39:42 +0300

    test:implement calculation method

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,9 @@
 
-// var sizeValue = document.getElementById("#sizeInput").value;
-// var crustValue = document.getElementById("#crustInput").value;
-// var toppingValue = document.getElementById("#topping2").value;
-// var checkout = document.getElementById("#button1")
-
-// function calculate(){
-//     if(sizeValue ==""){
-//         alert("Ensure you've selected size")
-//     }
-//     else if (crustValue==""){
-//         alert("Ensure you've selected crust")
-//     }
-//     else if(toppingValue==""){
-//         alert("Ensure you've selected topping")
-//     }
-//     else{
-//     document.getElementById("sum").innerHTML= sizeValue+crustValue+toppingValue
-//     }
-// }
-// checkout.addEventListener("click", function (e) {
-//     e.preventDefault();
-//  calculate()
-// });
-
-
-// business logic
-function Purchase(size, flavor, crust, topping, total) {
+function Purchase(size, crust, topping, totalPrice) {
     this.size = size;
-    this.flavor = flavor;
     this.crust = crust;
     this.topping = topping;
-    this.total = total;
+    this.totalPrice = totalPrice;
 }
 
 // user interface logic
@@ -39,69 +12,75 @@ $(document).ready(function () {
                 event.preventDefault();
 
                 let inputtedSize = $("#sizeInput").val();
-                let inputtedFlavor = $("#flavorInput").val();
-                let inputtedCrust = $("#crustInput").val();
+                    let inputtedCrust = $("#crustInput").val();
                 let inputtedTopping = [];
-
                 $.each($("input[name='topping1']:checked"), function () {
-                    inputtedTopping.push($(this).val());
+                    inputtedTopping.push($(this).val()); 
+                
                 });
-                console.log(inputtedTopping.join(","));
-                //  use switch statement as control flow for Purchase constructor
-                switch (size) {
-                    case "0":
-                        amount = 0;
-                        break;
-                    case "Large":
-                        amount = 1500;
-                        console.log(amount);
-                        break;
-                    case "Medium":
-                        amount = 900;
-                        console.log(amount);
-                        break;
-                    case "Regular":
-                        amount = 450;
-                        console.log(amount);
-                    default:
-                        console.log("invalid");
-                }
-                switch (crust) {
-                    case "0":
-                        crustPrice = 0;
-                        break;
-                    case "Flatbread":
-                        crustPrice = 50;
-                        break;
-                    case "Neapolitan pizza":
-                        crustPrice = 100;
-                        break;
-                    case "Stuffed crust":
-                        crustPrice = 150;
-                        break;
-                    case "Glutten-free":
-                        crustPrice = 200;
-                        break;
-                    case "Sicilian":
-                        crustPrice = 250;
-                        break;
-                    case "Deep-dish":
-                        crustPrice = 300;
-                        break;
-                    default:
-                        console.log("invalid")
-                }
-                let toppingCheckedValue = inputtedTopping.length * 30;
-                console.log(toppingCheckedValue);
-                var totalPrice = amount + crustPrice + inputtedTopping;
-                $("#sum").append(totalPrice);
+                // let numbers = inputtedTopping;
+                // let sum1 = numbers.reduce((x,y)=>x+y);
+                // console.log(sum1);
+                // //  use switch statement as control flow for Purchase constructor
+                // switch (size) {
+                //     case "0":
+                //         amount = 0;
+                //         break;
+                //     case "Large":
+                //         amount = 1500;
+                //         console.log(amount);
+                //         break;
+                //     case "Medium":
+                //         amount = 900;
+                //         console.log(amount);
+                //         break;
+                //     case "Regular":
+                //         amount = 450;
+                //         console.log(amount);
+                //     default:
+                //         console.log("invalid");
+                // }
+                // switch (crust) {
+                //     case "0":
+                //         crustPrice = 0;
+                //         break;
+                //     case "Flatbread":
+                //         crustPrice = 50;
+                //         break;
+                //     case "Neapolitan pizza":
+                //         crustPrice = 100;
+                //         break;
+                //     case "Stuffed crust":
+                //         crustPrice = 150;
+                //         break;
+                //     case "Glutten-free":
+                //         crustPrice = 200;
+                //         break;
+                //     case "Sicilian":
+                //         crustPrice = 250;
+                //         break;
+                //     case "Deep-dish":
+                //         crustPrice = 300;
+                //         break;
+                //     default:
+                //         console.log("invalid")
+                // }
+                // let toppingCheckedValue = inputtedTopping.length * 30;
+                // console.log(toppingCheckedValue);
+                // let totalPrice = inputtedSize+ inputtedCrust + inputtedTopping;
+                // document.getElementById("#sum").innerHTML = totalPrice;
                 // if ((size == "0") && (crust == "0")) {
                 //     alert("Select appropriate pizza size and crust")
                 // } else {
                  
                 // };
-
-        // var newOrder = new Purchase(inputtedSize, inputtedFlavor, inputtedCrust, inputtedTopping);
+        let totalPrice = parseInt(inputtedSize) + parseInt(inputtedCrust) + parseInt(inputtedTopping);
+        var newOrder = new Purchase(inputtedSize, inputtedCrust, inputtedTopping, totalPrice);
+        $("#outPutValues").append('</td><td id="selectSize">'+ newOrder.size + '</td><td id="selectCrust">'+newOrder.crust + '</td><td id="selectTopping">'+newOrder.topping+'</td><td id="total">'+newOrder.totalPrice+'</td></tr>');
+      console.log(newOrder.size);
+      console.log(newOrder.crust);
+      console.log(newOrder.topping);
+      console.log(newOrder.totalPrice)
 
         // $("ul#outputValues").append("<li><span class='contact'>" + newOrder.size + "</span></li>");
         // $("ul#outputValues").append("<li><span class='contact'>" + newOrder.flavor + "</span></li>");
@@ -112,9 +91,10 @@ $(document).ready(function () {
 
 
 
-        // $("#sizeInput").val("");
-        // $("#flavorInput").val("");
-        // $("#crustInput").val("");
+        $("#sizeInput").val("");
+        $("#flavorInput").val("");
+        $("#crustInput").val("");
+        $("outPutValues").append("")
 
 
 
